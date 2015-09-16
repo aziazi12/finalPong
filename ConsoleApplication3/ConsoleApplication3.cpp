58f0f9f833e10f77f4b9410e01d36232a29d5ef3
Aziz Aziz <aziz98366@gmail.com>
2015-09-16 11:49:21 -0700

    FINAL PONG VERISON TO WORK ON

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -2,7 +2,6 @@
 //
 
 #include "stdafx.h"
-#include "stdafx.h"
 
 #include <string>
 #include <windows.h>
@@ -15,11 +14,34 @@
 #include "GL/freeglut.h"
 #pragma comment(lib, "OpenGL32.lib")
 
+// window size
+const int width = 800;
+const int height = 640;
+const int interval = 1000 / 60;
 
-int width = 800;
-int height = 640;
-int interval = 1000 / 60;
+// paddle sizes
+const int paddleWidth = 10;
+const int paddleHeight = 80;
+const int speed = 7;
 
+
+// left paddle position
+float leftX = 10.0f;
+float leftY = 50.0f;
+
+//right paddle position
+float rightX =  width - paddleWidth - 10;
+float rightY = 50.0f;
+
+// ball
+float ballPosX = width / 2;
+float ballPosY = height / 2;
+float ballDirX = -1.0f;
+float ballDirY = 0.0f;
+const int ballSize = 15;
+const int ballSpeed = 2;
+
+// Makes the game 2D, might eventually make the game 3D
 void enable2D(int width, int height) {
 	glViewport(0, 0, width, height);
 	glMatrixMode(GL_PROJECTION);
@@ -29,19 +51,83 @@ void enable2D(int width, int height) {
 	glLoadIdentity();
 }
 
+// Draw the paddles
+void drawPaddle(float x, float y, float width, float height) {
+	glBegin(GL_QUADS);
+	glVertex2f(x, y);
+	glVertex2f(x + width, y);
+	glVertex2f(x + width, y + height);
+	glVertex2f(x, y + height);
+	glEnd();
+}
+
+// Draws everything and keeps it legit
 void draw() {
 	// clear (has to be done at the beginning)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
-	// ToDo: draw our scene
+	// paddles
+	drawPaddle(leftX, leftY, paddleWidth, paddleHeight);
+	drawPaddle(rightX, rightY, paddleWidth, paddleHeight);
+
+	// ball
+	drawPaddle(ballPosX - ballSize / 2, ballPosY- ballSize / 2, ballSize, ballSize);
 
 	// swap buffers (has to be done at the end)
 	glutSwapBuffers();
 }
 
+// ball movement
+void ballMovement()
+{
+	ballPosX += ballDirX * ballSpeed;
+	ballPosY += ballDirY * ballSpeed;
+
+	// hit by left paddle
+	if (ballPosX < leftX +width && ballPosX > leftX && ballPosY < leftY + height && ballPosY> leftY)
+	{
+		float temp = ((ballPosX - leftY) / paddleHeight) - 0.5f;
+		ballDirX = fabs(ballDirX); // force it to be positive
+		ballDirY = temp;
+
+	}
+
+}
+
+void controls()
+{
+
+	if (GetAsyncKeyState('W'))
+	{
+		leftY += speed;
+
+	}
+
+	if (GetAsyncKeyState('S'))
+	{
+		leftY -= speed;
+
+	}
+
+	if (GetAsyncKeyState('O'))
+	{
+		rightY += speed;
+	}
+
+	if (GetAsyncKeyState('L'))
+	{
+		rightY -= speed;
+	}
+
+}
+
+// Keeps everything updated
 void update(int value) {
-	// Call update() again in 'interval' milliseconds
+
+	controls();
+	ballMovement();
+	// Call update
 	glutTimerFunc(interval, update, 0);
 
 	// Redisplay frame
@@ -50,7 +136,7 @@ void update(int value) {
 
 
 int _tmain(int argc, char** argv) {
-	// initialize opengl (via glut)
+	// opengl with glut
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(width, height);
@@ -63,7 +149,6 @@ int _tmain(int argc, char** argv) {
 	// sets up 2d mode and draws to white
 	enable2D(width, height);
 	glColor3f(1.0f, 1.0f, 1.0f);
-
 
 	// starts the whole thing
 	glutMainLoop();
